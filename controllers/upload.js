a4b9640bb1e3aa4bb2241380fabacaedc7d74a30
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-03-11 00:07:09 +0200

    save image url and key in data base

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,6 +2,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const AWS=require('aws-sdk')
 const uuid=require('uuid/v1')
+const Upload = require('../models/Upload');
 
 const s3=new AWS.S3({
     accessKeyId:process.env.accessKeyId,
@@ -25,6 +26,7 @@ exports.upload = asyncHandler(async (req, res, next) => {
 
     //    contentType=file.mimetype
 
+    
 
 
        const params = {
@@ -33,15 +35,20 @@ exports.upload = asyncHandler(async (req, res, next) => {
         Expires: 60, 
         ResponseContentType: 'image/png'
       };
+
       
-      s3.getSignedUrl('getObject', params, function (err, url) {
-        if (url) 
+      s3.getSignedUrl('getObject', params, async (err, url) =>{
+        if (url) {
+           //save to data base 
+           const file = await Upload.create({url,key});
         res.status(201).json({
                 success: true,
-                data: {key,url}
-              })
-        else return next(new ErrorResponse(err, 400));;
-      });
+                // data: {url,key},
+                file
+              })}
+        else {return next(new ErrorResponse(err, 400))}
+      }
+      );
 
 
 
