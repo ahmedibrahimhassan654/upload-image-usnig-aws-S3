dd81196afa8851e8055f46ae805905e64f1ba65e
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-03-16 01:31:17 +0200

    using jest.fun to mock moddel

--- tests/unit/register.controller.test.js
+++ tests/unit/register.controller.test.js
@@ -1,9 +1,16 @@
 const { italic } = require('colors')
 const {register}=require('../../controllers/auth')
+const User =require('../../models/User')
+
+User.create=jest.fn()
 
 describe('register new user',()=>{
-    it('shoud register new user api',()=>{
+    it('shoud register new user api function',()=>{
         expect(typeof register).toBe('function')
+    })
+    it('should create new user in user model',()=>{
+        register()
+        expect(User.create).toBeCalled()
     })
 })
 
